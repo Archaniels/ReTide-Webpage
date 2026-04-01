c21550221e01fc4a71151fe40b6ac25dec80b9f0
Daniyal Arshaq S. <arshaqdaniyal@gmail.com>
2026-04-01 11:32:27 +0700

    please bisa 8

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,5 +21,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+        if (config('app.env') === 'production') {
+            config(['view.compiled' => '/tmp/storage/framework/views']);
+        }
     }
 }
