8ee4964e57f953e39fabe71afd77094fbebbc226
Daniyal Arshaq S. <arshaqdaniyal@gmail.com>
2026-05-06 08:14:07 +0700

    hotfix 1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         if (config('app.env') === 'production') {
             config(['view.compiled' => '/tmp/storage/framework/views']);
+            URL::forceScheme('https');
         }
     }
 }
