3af93f713dbab5deba9f7847e1475b6b3fa3dbce
Daniyal Arshaq S. <arshaqdaniyal@gmail.com>
2026-06-16 11:45:19 +0700

    hotfix: Laravel filesystem issue fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -63,9 +63,7 @@ return [
 
         "cloudinary" => [
             "driver" => "cloudinary",
-            "api_key" => env("CLOUDINARY_API_KEY"),
-            "api_secret" => env("CLOUDINARY_API_SECRET"),
-            "cloud_name" => env("CLOUDINARY_CLOUD_NAME"),
+            "url" => env("CLOUDINARY_URL"),
         ],
     ],
 
